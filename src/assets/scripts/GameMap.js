341caf55ae621992416d2c581ff0740697d6899b
YangYiFan <Coolfan1024@gmail.com>
2024-02-23 20:50:09 +0800

    蛇的逻辑；用户操控；

--- src/assets/scripts/GameMap.js
+++ src/assets/scripts/GameMap.js
@@ -1,4 +1,5 @@
 import { AcGameObject } from "./ACGameObject";
+import { Snake } from "./Snake";
 import { Wall } from "./wall";
 
 export class GameMap extends AcGameObject {
@@ -12,6 +13,44 @@ export class GameMap extends AcGameObject {
 
         this.inner_walls_count = 20;
         this.walls = [];
+
+        this.snakes = [
+            new Snake({ id: 0, color: '#4876EC', r: this.rows - 2, c: 1 }, this),
+            new Snake({ id: 1, color: '#F94848', r: 1, c: this.cols - 2 }, this),
+        ];
+
+    }
+
+    add_listening_events() {
+        this.ctx.canvas.focus();
+        const [snake0, snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === "w") snake0.set_dirction(0);
+            else if (e.key === "d") snake0.set_dirction(1);
+            else if (e.key === "s") snake0.set_dirction(2);
+            else if (e.key === "a") snake0.set_dirction(3);
+            else if (e.key === "ArrowUp") snake1.set_dirction(0);
+            else if (e.key === "ArrowRight") snake1.set_dirction(1);
+            else if (e.key === "ArrowDown") snake1.set_dirction(2);
+            else if (e.key === "ArrowLeft") snake1.set_dirction(3);
+
+
+        });
+    }
+    check_valid(cell) { //检测目标节点是否合法
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c)
+                return false;
+        }
+        for (const Snake of this.snakes) {
+            let k = Snake.cells.length;
+            if (!Snake.check_tail_increasing()) k--; //当蛇在本局中不增长时，忽略最后蛇尾重叠
+            for (let i = 0; i < k; i++) {
+                if (Snake.cells[i].r === cell.r && Snake.cells[i].c === cell.c)
+                    return false;
+            }
+        }
+        return true;
     }
     create_walls() {
         // 初始化表示墙的二维数组,给四周加上墙
@@ -32,9 +71,10 @@ export class GameMap extends AcGameObject {
             for (; ;) {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[this.rows-1-r][this.cols-1-c]) continue;
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
                 // 确保蛇的出生点没有墙
                 if ((r === this.rows - 2 && c === 1) || (c === this.cols - 2 && r === 1)) continue;
+                // 中心对称
                 g[r][c] = true;
                 g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
@@ -68,10 +108,20 @@ export class GameMap extends AcGameObject {
         for (let i = 0; i < 10000; i++)
             if (this.create_walls())
                 break;
-
-
-        // this.create_walls();
+        this.add_listening_events();
     }
+
+    check_snake_ready() {
+        for (const Snake of this.snakes) {
+            if (Snake.status === "die") return false;
+            if (Snake.direction === -1) return false;
+        }
+        return true;
+    }
+    next_step() {
+        for (const Snake of this.snakes) Snake.next_step();
+    }
+
     resize() {
         this.L = parseInt(Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows));
         this.ctx.canvas.width = this.L * this.cols;
@@ -79,6 +129,7 @@ export class GameMap extends AcGameObject {
     }
     update() {
         this.resize();
+        if (this.check_snake_ready()) this.next_step();
         this.render();
     }
 
