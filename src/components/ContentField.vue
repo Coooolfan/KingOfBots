b7864f913bcba2dd078bdbba308fa5e8cd66ee98
YangYiFan <Coolfan1024@gmail.com>
2025-01-07 16:36:40 +0800

    feat: 添加操作者提示信息

--- src/components/ContentField.vue
+++ src/components/ContentField.vue
@@ -4,19 +4,30 @@
             <div class="card-body">
                 <slot></slot>
             </div>
+            <p class="tips">{{ computedTips }}</p>
         </div>
     </div>
 </template>
 
 <script>
+import { computed } from 'vue';
 import { useStore } from 'vuex';
 
 export default {
+    setup() {
+        return {
+            tips: "这是一个内容区域，可以放置游戏地图、匹配按钮、结果面板等内容"
+        }
+    },
     computed: {
         computedBackgroundColor() {
             if (useStore().state.pk.status === "matching") return "white"
             return Number(useStore().state.user.id) === Number(useStore().state.pk.b_id) ? '#fff6f6' : "#f6f8fe";
         },
+        computedTips() {
+            if (useStore().state.pk.status !== "playing") return ""
+            return Number(useStore().state.user.id) === Number(useStore().state.pk.b_id) ? '你是红方' : "你是蓝方";
+        }
     },
 };
 </script>
@@ -25,5 +36,10 @@ export default {
 div.content-field {
     margin-top: 20px;
     margin-bottom: 20px;
+}
+.tips{
+    text-align: center;
+    font-size: xx-large;
+    color: #666;
 }
 </style>
